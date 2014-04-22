5d5d36626911e18967b9b9ddfa58f71a6ad1f814
jerleng <jerleng@gmail.com>
2014-04-22 16:24:36 -0700

    Update TimeSeriesGrailsPlugin.groovy

--- TimeSeriesGrailsPlugin.groovy
+++ TimeSeriesGrailsPlugin.groovy
@@ -7,7 +7,7 @@ class TimeSeriesGrailsPlugin {
 	def author = "Jeremy Leng"
 	def authorEmail = "jleng@bcap.com"
 	def description = 'Provides a simplified service for reading/writing timeseries data and storing it in a variety of time resolutions. Read method output is intended to support javascript charting libraries. The plugin defines an interface for pluggable storage providers and includes an implementation in-memory storage provider'
-	def documentation = "http://grails.org/plugin/time-series"
+	def documentation = "https://github.com/bertramdev/timeseries"
 	def license = "APACHE"
 	def organization = [ name: "BertramLabs", url: "http://www.bertramlabs.com/" ]
 	def issueManagement = [system: "GITHUB", url: "https://github.com/bertramdev/timeseries/issues" ]
